dfeaf4e2fae0e74ee8b7c23ecaab4b1318cef403
freejoe <joe.wong2016@outlook.com>
2021-06-16 15:57:06 +0800

    12864lcd ok

--- Marlin/src/pins/ramps/pins_LONGER_LGT_KIT_V1.h
+++ Marlin/src/pins/ramps/pins_LONGER_LGT_KIT_V1.h
@@ -28,6 +28,44 @@
 
 #define BOARD_INFO_NAME "LONGER LGT KIT V1.0"
 
+#include "pins_RAMPS.h"
+
+#undef SERVO0_PIN
+#undef Z_MIN_PIN
+#undef FIL_RUNOUT_PIN
+#ifdef SD_DETECT_PIN
+  #undef SD_DETECT_PIN
+#endif
+
+#ifdef KILL_PIN
+  #undef KILL_PIN
+#endif
+
+#ifdef CR10_STOCKDISPLAY
+  #undef LCD_PINS_RS
+  #undef LCD_PINS_ENABLE
+  #undef LCD_PINS_D4
+  #undef BTN_EN1
+  #undef BTN_EN2
+
+  #define LCD_PINS_RS          5
+  #define LCD_PINS_ENABLE      4
+  #define LCD_PINS_D4          6
+  #define BTN_EN1              18
+  #define BTN_EN2              19
+
+#endif
+
+#ifdef BEEPER_PIN
+  #undef BEEPER_PIN
+  #define BEEPER_PIN           11
+#endif
+
+#ifdef BTN_ENC
+  #undef BTN_ENC
+  #define BTN_ENC              15
+#endif
+
 // #define LK1_PRO
 #define LK4_PRO
 
@@ -40,23 +78,24 @@
 // Servos
 //
 #if defined(LK1_PRO)
-#define SERVO0_PIN          11
+  #define SERVO0_PIN          11
 #elif defined(LK4_PRO)
-#define SERVO0_PIN          7
+  #define SERVO0_PIN          7
 #endif
 
 //
 // Limit Switches
 //
 #if defined(LK1_PRO)
-  #define Z_STOP_PIN          11
+  #define Z_MIN_PIN          11
 #elif defined(LK4_PRO)
-  #define Z_STOP_PIN          35
+  #define Z_MIN_PIN          35
 #endif
 
 #define SD_DETECT_PIN   49
 #define FIL_RUNOUT_PIN  2
 
-#include "pins_RAMPS.h"
+
+
 
 
